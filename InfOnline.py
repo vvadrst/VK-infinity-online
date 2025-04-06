59f2576ebd40706eaf286e1b6e5ea61e4baafbda
abstrxctive <murisa833@gmail.com>
2025-04-06 22:09:41 +0300

    Update InfOnline.py

--- InfOnline.py
+++ InfOnline.py
@@ -1,17 +1,25 @@
 import time
 import vk_api
+from threading import Thread
 from config import TOKEN
 
 # В файле config.py укажите токен авторизации страницы
-session = vk_api.VkApi(token=TOKEN)
-vk = session.get_api()
+def get_vk_session(acces_token, i):
+    session = vk_api.VkApi(token=acces_token)
+    vk_session = session.get_api()
 
-while True:
-    try:
-        print('Онлайн режим активирован')
-        vk.account.setOnline()
-# Каждые 5 минут (300) секунд будет активироваться метод setOnline
-        time.sleep(300)
-    except Exception as e:
-        print(f'Что-то пошло не так: {e}')
-# запустить командой python3 InfOnline.py
+    while True:
+        try:
+            print(f'Онлайн режим активирован для аккаунта: {i}')
+            vk_session.account.setOnline() # 5 мин == 300 сек
+            time.sleep(5)
+        except Exception as e:
+            print(f'Что-то пошло не так: {e}')
+            time.sleep(30) # Пауза при ошибке
+
+# Создаем и запускаем потоки для каждого токена
+thread_1 = Thread(target=get_vk_session, args=(TOKEN, 1))
+thread_1.start()
+
+# Ожидаем завершения потоков (хотя они бесконечные)
+thread_1.join()
